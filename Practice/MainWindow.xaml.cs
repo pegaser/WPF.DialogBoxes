de6a2bf7d54d13f487df42aeff9a62ece68ba008
pegaser <sperezgabriel@gmail.com>
2022-10-10 17:52:18 -0500

    10 OCTUBRE 2022

---
+++ Practice/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnOpenFile.Click += BtnOpenFile_Click;
+        }
+
+        private void BtnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog myDialog = new OpenFileDialog();
+            myDialog.Filter = "Image Files(*.txt)|*.TXT";
+            myDialog.CheckFileExists = true;
+            myDialog.Multiselect = true;
+            if (myDialog.ShowDialog() == true)
+            {
+                foreach (string file in myDialog.FileNames)
+                {
+                    Process.Start(new ProcessStartInfo { FileName = file, UseShellExecute = true });
+                }
+            }
+        }
+    }
+}
